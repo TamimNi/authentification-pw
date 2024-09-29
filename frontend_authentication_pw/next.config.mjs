95bb374fb948dae336f5b634d362de35a6163e7b
TamimNi <tamim.aut@gmail.com>
2024-09-29 23:05:24 +0200

    nextjs with https

--- frontend_authentication_pw/next.config.mjs
+++ frontend_authentication_pw/next.config.mjs
@@ -1,4 +1,14 @@
+import fs from 'fs';
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Any other Next.js config options go here
+
+  // Custom Webpack Dev Middleware to configure HTTPS
+  webpackDevMiddleware: (config) => {
+    return config;
+  },
+};
 
 export default nextConfig;
